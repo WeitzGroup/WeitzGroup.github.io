9ace5bb4818d8ca806aef7c7148e862b760aae67
Stephen Beckett <beckett.stephenj@gmail.com>
2020-04-08 12:43:24 -0400

    Add files via upload

--- js/Rplot.js
+++ js/Rplot.js
@@ -1,7 +1,7 @@
 (function($) {
     $(document).ready(function() {
 	$('#Rplot').scianimator({
-	    'images':  ['cases/001cases.jpg', 'cases/002cases.jpg', 'cases/003cases.jpg', 'cases/004cases.jpg', 'cases/005cases.jpg', 'cases/006cases.jpg', 'cases/007cases.jpg', 'cases/008cases.jpg', 'cases/009cases.jpg', 'cases/010cases.jpg', 'cases/011cases.jpg', 'cases/012cases.jpg', 'cases/013cases.jpg', 'cases/014cases.jpg', 'cases/015cases.jpg', 'cases/016cases.jpg', 'cases/017cases.jpg', 'cases/018cases.jpg', 'cases/019cases.jpg', 'cases/020cases.jpg', 'cases/021cases.jpg', 'cases/022cases.jpg', 'cases/023cases.jpg', 'cases/024cases.jpg', 'cases/025cases.jpg', 'cases/026cases.jpg', 'cases/027cases.jpg', 'cases/028cases.jpg', 'cases/029cases.jpg', 'cases/030cases.jpg', 'cases/031cases.jpg', 'cases/032cases.jpg', 'cases/033cases.jpg', 'cases/034cases.jpg', 'cases/035cases.jpg'],
+	    'images':  ['cases/001cases.jpg', 'cases/002cases.jpg', 'cases/003cases.jpg', 'cases/004cases.jpg', 'cases/005cases.jpg', 'cases/006cases.jpg', 'cases/007cases.jpg', 'cases/008cases.jpg', 'cases/009cases.jpg', 'cases/010cases.jpg', 'cases/011cases.jpg', 'cases/012cases.jpg', 'cases/013cases.jpg', 'cases/014cases.jpg', 'cases/015cases.jpg', 'cases/016cases.jpg', 'cases/017cases.jpg', 'cases/018cases.jpg', 'cases/019cases.jpg', 'cases/020cases.jpg', 'cases/021cases.jpg', 'cases/022cases.jpg', 'cases/023cases.jpg', 'cases/024cases.jpg', 'cases/025cases.jpg', 'cases/026cases.jpg', 'cases/027cases.jpg', 'cases/028cases.jpg', 'cases/029cases.jpg', 'cases/030cases.jpg', 'cases/031cases.jpg', 'cases/032cases.jpg', 'cases/033cases.jpg', 'cases/034cases.jpg', 'cases/035cases.jpg','cases/036cases.jpg','cases/037cases.jpg'],
 	    'width': 1000,
 	    'delay': 800,
 	    'loopMode': 'loop'
