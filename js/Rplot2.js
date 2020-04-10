78c79a7bbee86a251796a16aadc2c62578a3d5cb
Stephen Beckett <beckett.stephenj@gmail.com>
2020-04-10 12:27:46 -0400

    Update Rplot2.js

--- js/Rplot2.js
+++ js/Rplot2.js
@@ -10,7 +10,7 @@ function addZ(n) {
         	imgs[i] = "deaths/" + addZ(i + 1) + "deaths.png";
       	}
 	$('#Rplot2').scianimator({
-	    'images':  ['deaths/001deaths.jpg', 'deaths/002deaths.jpg', 'deaths/003deaths.jpg', 'deaths/004deaths.jpg', 'deaths/005deaths.jpg', 'deaths/006deaths.jpg', 'deaths/007deaths.jpg', 'deaths/008deaths.jpg', 'deaths/009deaths.jpg', 'deaths/010deaths.jpg', 'deaths/011deaths.jpg', 'deaths/012deaths.jpg', 'deaths/013deaths.jpg', 'deaths/014deaths.jpg', 'deaths/015deaths.jpg', 'deaths/016deaths.jpg', 'deaths/017deaths.jpg', 'deaths/018deaths.jpg', 'deaths/019deaths.jpg', 'deaths/020deaths.jpg', 'deaths/021deaths.jpg', 'deaths/022deaths.jpg', 'deaths/023deaths.jpg', 'deaths/024deaths.jpg', 'deaths/025deaths.jpg', 'deaths/026deaths.jpg', 'deaths/027deaths.jpg', 'deaths/028deaths.jpg', 'deaths/029deaths.jpg', 'deaths/030deaths.jpg', 'deaths/031deaths.jpg', 'deaths/032deaths.jpg', 'deaths/033deaths.jpg', 'deaths/034deaths.jpg', 'deaths/035deaths.jpg','deaths/036deaths.jpg','deaths/037deaths.jpg'],
+	    'images':  imgs,
 	    'width': 1000,
 	    'delay': 800,
 	    'loopMode': 'loop'
