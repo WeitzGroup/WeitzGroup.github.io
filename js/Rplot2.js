81b2d97dafc5c53e469d2d662cd915630d9725a5
Stephen Beckett <beckett.stephenj@gmail.com>
2020-04-12 11:54:10 -0400

    Update Rplot2.js

--- js/Rplot2.js
+++ js/Rplot2.js
@@ -4,7 +4,7 @@ function addZ(n) {
 
 (function($) {
     $(document).ready(function() {
-	var imgs = Array(39);
+	var imgs = Array(41);
 	for (i = 0; ; i++) {
         	if (i == imgs.length) break;
         	imgs[i] = "deaths/" + addZ(i + 1) + "deaths.png";
