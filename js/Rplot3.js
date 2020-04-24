e811a976b1ab741fa5ed828fb03f7dc752c8296a
Stephen Beckett <beckett.stephenj@gmail.com>
2020-04-24 17:10:27 -0400

    Add files via upload

---
+++ js/Rplot3.js
@@ -0,0 +1,20 @@
+function addZ(n) {
+  return (n<10? '00' : n<100? '0' : '') + n;
+}
+
+(function($) {
+    $(document).ready(function() { 
+	var imgs = Array(53);
+       	for (i = 0; ; i++) {
+        	if (i == imgs.length) break;
+        	imgs[i] = "mix/" + addZ(i + 1) + "mix.png";
+      	}
+	$('#Rplot3').scianimator({
+	    'images':  imgs,
+	    'width': 500,
+	    'delay': 800,
+	    'loopMode': 'loop'
+	});
+	$('#Rplot3').scianimator('play');
+    });
+})(jQuery);
