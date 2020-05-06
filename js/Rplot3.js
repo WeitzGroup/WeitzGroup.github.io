c3f9574333635ef594f0983c5e31d96c59033f9f
Stephen Beckett <beckett.stephenj@gmail.com>
2020-05-06 13:15:41 -0400

    Update Rplot3.js

--- js/Rplot3.js
+++ js/Rplot3.js
@@ -4,7 +4,7 @@ function addZ(n) {
 
 (function($) {
     $(document).ready(function() { 
-	var imgs = Array(57);
+	var imgs = Array(65);
        	for (i = 0; ; i++) {
         	if (i == imgs.length) break;
         	imgs[i] = "mix/" + addZ(i + 1) + "mix.png";
